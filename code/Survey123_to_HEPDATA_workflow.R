e6f99e809b52c7534fa6f22ca44aca7d7d8468e2
scottfjennings <scott.f.jennings@gmail.com>
2021-08-12 11:01:32 -0700

    small edits, removing text that belongs in vignette

--- code/Survey123_to_HEPDATA_workflow.R
+++ code/Survey123_to_HEPDATA_workflow.R
@@ -1,8 +1,11 @@
 
 
-# entire workflow for processing Survey123 data
+# recommended script for the entire workflow for processing Survey123 data
+
+# setup: run this chunk for every step ---- 
 library(tidyverse)
 library(here)
+library(devtools)
 
 source(here("code/survey123_utility_functions.r"))
 
@@ -11,63 +14,45 @@ source("C:/Users/scott.jennings/Documents/Projects/R_general/utility_functions/b
 zyear = 2020
 #
 # step 1, convert Survey123 data to more-usable format ----
-# this step uses functions from survey123_wrangle.R
 source(here("code/step1_wrangle_survey123.r"))
 
-# add table with "expected" number of species (query HEPDATA)
-# change "code" to "site.code"
 wrangled_s123 <- read_s123(zyear, add.test.data = TRUE) %>% 
-  filter(useforsummary == "y") %>% # 
+  filter(useforsummary == "y") %>% 
   fix_s123_names() %>% 
   fix_s123_date_fields() %>% 
   add_multiple_survey_num() %>% 
   wrangle_s123()
   
-
-
-wrangled_s123 %>% saveRDS(paste("data/wrangled/wrangled_s123", zyear, sep = "_"))
+wrangled_s123 %>% saveRDS(here(paste("data/wrangled/wrangled_s123", zyear, sep = "_")))
  
 # step 1.1, check for any data issues, and data summary helpers to prepare for manual screening ----
-
-# the functions called in this section load the appropriate wrangled_s123,
-# other checks require loading wrangled_s123 directly
 wrangled_s123 <- readRDS(paste("data/wrangled/wrangled_s123", zyear, sep = "_"))
 #
-# 1.1.1 which species have nested in each colony? ---- 
-# for 1, a few, or all colonies, check which species have nested there and peak active nests for each year.
+# which species have nested in each colony? ---- 
 check_nesting_history(2020, c(53), screened.s123 = FALSE) %>% # from survey123_utility_functions.R
   pivot_wider(id_cols = c(code, site.name, year), values_from = total.nests, names_from = species) %>% 
   view()
 
-#
- 
-# 1.1.2 check start and end date match ----
+# check start and end date match ----
 # if nrow = 0, no problem
 wrangled_s123$dates %>% 
   filter(as.Date(start, tz = Sys.timezone(location = TRUE)) != as.Date(end, tz = Sys.timezone(location = TRUE))) %>% 
   nrow()
   
-# 1.1.3 check for any surveys entered as happening at night, not yet developed, needed? ----
 
-# 1.1.4 check for multiple surveys at same site and same date ----
-# such visits are fine and consistent with the field protocol, and the screening code should handle these multiple surveys fine, but nevertheless good to be aware of before proceeding with screening so you can check to make sure all the data made it through screening appropriately
-
+# check for multiple surveys at same site and same date ----
 wrangled_s123$dates %>% filter(num.surveys.this.date > 1) %>% view()
 
-# add check for correct data types, values within expected ranges
-
-# 1.1.5 check observer X date X colony   ----
+# check observer X date X colony   ----
 check_expected_observers(zyear) %>% # from survey123_utility_functions.R
   filter(only.unexpected.observer == TRUE) %>% 
   view()
 
 
-# check for time span between surveys, not yet developed, needed? ----
- 
-# step 2, output wrangled Survey123 data into Season Summary sheets. ----
-# create a sheet for each instance where a species nested in a colony
 
-# list of all species in each colony this year. use this list to make season summary sheets only for species that actually nested at each colony
+# step 2, output wrangled Survey123 data into Season Summary sheets. ----
+
+# list of all species in each colony this year. 
 colony_spp <- readRDS(paste("data/wrangled/wrangled_s123", zyear, sep = "_"))$nests %>% 
   filter(total.nests > 0) %>% 
   distinct(code, species) %>% 
@@ -99,22 +84,12 @@ system.time(pmap(.l = list(file = here("code/step2_wrangled_s123_to_season_summa
 # creating the 6 test data sheets takes ~30 seconds
 
 
-# step 3, NO CODE - MANUALLY SCREEN SEASON SUMMARY SHEETS. ---- 
-  # when a sheet has been screened the first time, change its name from [code]_[year]_[species].docx to [code]_[year]_[species]_screened1.docx
-  # if a sheet is screened a second time, change its name from [code]_[year]_[species]_screened1.docx to [code]_[year]_[species]_screened2.docx
-  # files must be .docx. .doc will not work without downloading LibreOffice software
-# step 3.1, check season summary sheets needing to be screened ---- 
-  # this mostly won't be needed, since it will be easy to visually id unscreened sheets in the folder. if the above renaming is NOT done, then this code will not perform as designed/expected
-  grep('screened', list.files(here(paste("season_summary_forms/", zyear, "/", sep = ""))), invert=TRUE, value=TRUE)
-  
-  
-   
-# step 4, extract tables from screened Season Summary Sheets ----
-  # uses functions in extract_screened_season_summary.R
-source(here("code/step4_extract_screened_season_summary.r"))
+# step 3, NO CODE - STOP AND MANUALLY SCREEN SEASON SUMMARY SHEETS. ---- 
 
-  screened_seas_summ_files <- list.files(paste("season_summary_forms/", zyear, "/", sep = ""), pattern = "screened")
-# these functions (defined in extract_screened_season_summary.R) loop through each .docx file and extract the pertinent table, then assemble those into a single data frame. Bundling those data frames into a list brings the data back to the same structure and names as wrangled_s123, so that a log of screening changes can be easily made (step 5)
+# step 4, extract tables from screened Season Summary Sheets ----
+source_url("https://github.com/scottfjennings/Survey123_to_HEPDATA/blob/main/code/step4_extract_screened_season_summary.R")
+
+
 # note, running the code below will overwrite any previous version of screened_s123, it will not append new records to the previous version. This generally shouldn't be a problem (I can't think of a realistic scenario), but it is something to be aware of.  
 
 screened_s123 <- list(screen.log = map2_df(zyear, screened_seas_summ_files, get_screening_log),
@@ -130,10 +105,8 @@ screened_s123 <- list(screen.log = map2_df(zyear, screened_seas_summ_files, get_
 saveRDS(screened_s123, here(paste("data/screened/screened_s123_", zyear, sep = "")))
  
 # step 5 generate screening change log ----
-# uses functions defined in step5_make_screening_change_log.R
 source(here("code/step5_make_screening_change_log.R"))
 
-screened_sheets <- get_screened_sheets(zyear)
 
 track_changes_s123 <- list(screen.log = readRDS(here(paste("data/screened/screened_s123_", zyear, sep = "")))[["screen.log"]],
                            observers.effort = make_track_change_tables(zyear, "observers.effort"),
@@ -155,6 +128,11 @@ track_changes_s123 <- list(screen.log = readRDS(here(paste("data/screened/screen
                              mutate(date = as.Date(date)) %>% 
                              arrange(code, species, date)
 )
+
+
+track_changes_s123$nests %>% 
+  filter(grepl("changed", changelog)) %>% view()
+
 
 
 saveRDS(track_changes_s123, here(paste("data/track_changes/track_changes_s123_", zyear, sep = ""))) 

8741d955767cec8dafcd8560339b8cc757c80484
scottfjennings <scott.f.jennings@gmail.com>
2021-10-14 15:29:23 -0700

    refining code

--- code/step5_make_screening_change_log.R
+++ code/step5_make_screening_change_log.R
@@ -2,21 +2,16 @@
 
 # compare screened to unscreened to generate a list of changes made during screening
 
-get_screened_sheets <- function(zyear) {
-screened_sheets <- list.files(paste("season_summary_forms/", zyear, "/", sep = "")) %>% 
-  data.frame() %>% 
-  rename(file.name = 1) %>% 
-  mutate(file.name = gsub(".docx", "", file.name)) %>% 
-  separate(file.name, into = c("code", "year", "species", "screened"), sep = "_") %>% 
-  mutate(summary.sheet.made = TRUE,
-         screened = ifelse(is.na(screened), "no", screened)) %>% 
-  mutate(across(everything(), as.character)) %>% 
-  select(-year)
+get_screened_col_spp <- function(zyear) {
+screened_col_spp <- readRDS(here(paste("data/screened/screened_s123_", zyear, sep = "")))$screen.log %>%
+  mutate(screened = ifelse(screener.1 != "not screened", TRUE, FALSE)) %>% 
+  filter(screened == TRUE) %>% 
+  select(code, species, screened)
 }
 
 
 make_track_change_tables <- function(zyear, ztable) {
-screened_sheets <- get_screened_sheets(zyear)  
+screened_sheets <- get_screened_col_spp(zyear)  
   
 # get wrangled table 
 wrangled_table <- readRDS(here(paste("data/wrangled/wrangled_s123_", zyear, sep = "")))[[ztable]] %>%  
@@ -25,21 +20,20 @@ wrangled_table <- readRDS(here(paste("data/wrangled/wrangled_s123_", zyear, sep 
 
 # get screened table 
 screened_table <- readRDS(here(paste("data/screened/screened_s123_", zyear, sep = "")))[[ztable]] %>% 
+  data.frame() %>% 
   ungroup() %>% 
   mutate(record.in.screened = TRUE) %>% 
   mutate(across(everything(), as.character))
 
 table_changelog <- full_join(wrangled_table, screened_table) %>% 
-  full_join(screened_sheets) %>% # join with screen_log to have screening notes and confirmation that this colony X species was screened (assuming good record keeping during screening process) 
-  mutate(changelog = case_when(record.in.wrangled == TRUE & is.na(summary.sheet.made) ~ "no summary sheet",
-                               #species.not.nesting == TRUE ~ "species not nesting",
-                               screened == "no" ~ "not screened",
-                               grepl("screened", screened) & record.in.wrangled == TRUE & is.na(record.in.screened) ~ "screened, changed from",
-                               grepl("screened", screened) & is.na(record.in.wrangled) & record.in.screened == TRUE ~ "screened, changed to",
-                               grepl("screened", screened) & record.in.wrangled == TRUE & record.in.screened == TRUE ~ "screened, no change")) %>% 
+  full_join(screened_sheets) %>% # join with screen_log to have screening notes and confirmation that this colony X species was screened (assuming good record keeping during screening process)
+  arrange(code, species, date) %>% 
+  mutate(changelog = case_when(screened == TRUE & record.in.wrangled == record.in.screened ~ "screened, no change",
+                               screened == TRUE & record.in.wrangled == TRUE & is.na(record.in.screened) ~ "screened, changed from",
+                               screened == TRUE & is.na(record.in.wrangled) & record.in.screened == TRUE ~ "screened, changed to")) %>% 
   mutate(code = as.numeric(code)) %>% 
   select(code, species, everything()) %>% 
-  relocate(c(record.in.wrangled, summary.sheet.made, record.in.screened, screened, changelog), .after = last_col())
+  relocate(c(record.in.wrangled, record.in.screened, screened, changelog), .after = last_col())
 return(table_changelog)
 }
 

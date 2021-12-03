0d448e94b18832158e1f4c2461624a899c61e1d5
scottfjennings <scott.f.jennings@gmail.com>
2021-12-02 17:43:28 -0800

    turns out we need to handle tables with and without dates separately for the change log

--- code/step5_make_screening_change_log.R
+++ code/step5_make_screening_change_log.R
@@ -10,7 +10,36 @@ screened_col_spp <- readRDS(here(paste("data/screened/screened_s123_", zyear, se
 }
 
 
+
 make_track_change_tables <- function(zyear, ztable) {
+screened_sheets <- get_screened_col_spp(zyear)  
+  
+# get wrangled table 
+wrangled_table <- readRDS(here(paste("data/wrangled/wrangled_s123_", zyear, sep = "")))[[ztable]] %>%  
+  mutate(record.in.wrangled = TRUE) %>% 
+  mutate(across(everything(), as.character))
+
+# get screened table 
+screened_table <- readRDS(here(paste("data/screened/screened_s123_", zyear, sep = "")))[[ztable]] %>% 
+  data.frame() %>% 
+  ungroup() %>% 
+  mutate(record.in.screened = TRUE) %>% 
+  mutate(across(everything(), as.character))
+
+table_changelog <- full_join(wrangled_table, screened_table) %>% 
+  full_join(screened_sheets) %>% # join with screen_log to have screening notes and confirmation that this colony X species was screened (assuming good record keeping during screening process)
+  arrange(code, species) %>% 
+  mutate(changelog = case_when(screened == TRUE & record.in.wrangled == record.in.screened ~ "screened, no change",
+                               screened == TRUE & record.in.wrangled == TRUE & is.na(record.in.screened) ~ "screened, changed from",
+                               screened == TRUE & is.na(record.in.wrangled) & record.in.screened == TRUE ~ "screened, changed to")) %>% 
+  mutate(code = as.numeric(code)) %>% 
+  select(code, species, everything()) %>% 
+  relocate(c(record.in.wrangled, record.in.screened, screened, changelog), .after = last_col())
+return(table_changelog)
+}
+
+
+make_track_change_date_tables <- function(zyear, ztable) {
 screened_sheets <- get_screened_col_spp(zyear)  
   
 # get wrangled table 

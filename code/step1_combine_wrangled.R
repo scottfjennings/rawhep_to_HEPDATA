eb2b107a4706905467365cf0d8845421c3840d8a
scottfjennings <scott.f.jennings@gmail.com>
2021-12-10 13:21:19 -0800

    initial commit

---
+++ code/step1_combine_wrangled.R
@@ -0,0 +1,35 @@
+
+
+
+library(tidyverse)
+library(here)
+
+
+wrangled_alcatraz <- readRDS(here("data/wrangled/wrangled_alcatraz_2021"))
+
+
+wrangled_s123 <- readRDS(here("data/wrangled/wrangled_s123_2021"))
+
+
+wrangled_raw <- list(dates = rbind(wrangled_s123$dates %>% 
+                                     mutate(species = NA),
+                                   wrangled_alcatraz$dates),
+                     observers.effort = rbind(wrangled_s123$observers.effort %>% 
+                                     mutate(species = NA),
+                                              wrangled_alcatraz$observers.effort),
+                     nests = rbind(wrangled_s123$nests,
+                                   wrangled_alcatraz$nests),
+                     stages = rbind(wrangled_s123$stages,
+                                   wrangled_alcatraz$stages),
+                     brood.sizes = rbind(wrangled_s123$brood.sizes,
+                                   wrangled_alcatraz$brood.sizes),
+                     predators = rbind(wrangled_s123$predators,
+                                   wrangled_alcatraz$predators),
+                     disturbance = rbind(wrangled_s123$disturbance,
+                                   wrangled_alcatraz$disturbance),
+                     notes = rbind(wrangled_s123$notes,
+                                   wrangled_alcatraz$notes))
+
+
+wrangled_raw %>% 
+  saveRDS(here("data/wrangled/wrangled_raw_2021"))

371fdebbc3652cf51eca8efbac4607b9bad68869
scottfjennings <scott.f.jennings@gmail.com>
2021-12-15 08:20:04 -0800

    wrap in function

--- code/step1_combine_wrangled.R
+++ code/step1_combine_wrangled.R
@@ -1,10 +1,15 @@
 
 
 
-library(tidyverse)
-library(here)
 
-
+#' Combine wrangled HEP data objects.
+#'
+#' @return Object with the same structure as each individual wrangled_ object
+#' @export
+#'
+#' @examples
+#' combine_wrangled_hep %>% saveRDS(here("data/wrangled/wrangled_raw_2021"))
+combine_wrangled_hep <- function() {
 wrangled_alcatraz <- readRDS(here("data/wrangled/wrangled_alcatraz_2021"))
 
 
@@ -30,6 +35,7 @@ wrangled_raw <- list(dates = rbind(wrangled_s123$dates %>%
                      notes = rbind(wrangled_s123$notes,
                                    wrangled_alcatraz$notes))
 
+return(wrangled_raw)
 
-wrangled_raw %>% 
-  saveRDS(here("data/wrangled/wrangled_raw_2021"))
+}
+

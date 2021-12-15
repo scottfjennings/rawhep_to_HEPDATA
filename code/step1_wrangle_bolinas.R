eee97bddebce2e91bef18866d3fb4df00050c105
scottfjennings <scott.f.jennings@gmail.com>
2021-12-15 10:14:55 -0800

    initial. skeleton

---
+++ code/step1_wrangle_bolinas.R
@@ -0,0 +1,82 @@
+
+
+
+
+# now converting to wrangled format ----
+# first the dates table ----
+# "date"
+# "code"
+# "multiple.survey.num"
+# "start"
+# "end"
+# "num.surveys.this.date"
+# "complete.count" 
+
+# then observers.effort ----
+# "code"
+# "colony"
+# "observers"
+# "total.days"
+# "total.surveys"
+# "total.hours"
+
+# nests ----
+# "code"
+# "date"
+# "multiple.survey.num"
+# "species"
+# "total.nests"
+# "complete.count"
+# "peak.active"
+# "obs.initials"
+
+
+# stages ----
+# "code"
+# "date"
+# "multiple.survey.num"
+# "species"
+# "num.nests"
+# "stage"
+# "complete.count"
+# "which.rop"
+
+
+# brood.sizes ----
+# "code"
+# "date"
+# "multiple.survey.num"
+# "species"
+# "num.nests"
+# "brd"
+# "brd.size.date"
+# "stage5.nests"
+
+
+# predators ----
+# "code"
+# "date"
+# "multiple.survey.num"
+# "species"
+# "predator.species"
+# "present"
+# "nesting"
+
+
+# disturbance ----
+# "code"
+# "date"
+# "multiple.survey.num"
+# "species"
+# "obs.inf"
+# "type"
+# "result"
+# "description"
+
+# notes ----
+# "code"
+# "date"
+# "multiple.survey.num"
+# "species"
+# "note.type"
+# "notes"

2bd4e9f7e1de5c8ccd4c540f020948c99cdbacff
scottfjennings <scott.f.jennings@gmail.com>
2022-02-23 19:41:28 -0800

    was losing the most important notes!!

--- code/step4_extract_screened_season_summary.R
+++ code/step4_extract_screened_season_summary.R
@@ -197,7 +197,7 @@ get_notes <- function(zyear, zfile) {
     mutate(code = as.numeric(code),
            multiple.survey.num = ifelse(multiple.survey.num == "" | is.na(multiple.survey.num), 1, multiple.survey.num),
            multiple.survey.num = as.numeric(multiple.survey.num)) %>% 
-    filter(date != "")
+    filter(date != "" | note.type == "for.HEPDATA")
   }
 
 # all_notes <- map2_df(zyear, seas_summ_files, get_notes) %>% distinct() # should be duplicated for each species, distinct will remove dups

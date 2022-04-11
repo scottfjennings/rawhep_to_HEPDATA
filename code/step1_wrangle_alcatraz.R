22716db59def16ba4b4312a395d05ae8cf105586
scottfjennings <scott.f.jennings@gmail.com>
2022-04-11 11:08:02 -0700

    maek script into functions, document in ROxygen style

--- code/step1_wrangle_alcatraz.R
+++ code/step1_wrangle_alcatraz.R
@@ -1,78 +1,112 @@
 
 ## this file contains code and instructions for converting Alcatraz nest monitoring data to ACR's HEP data format
 
-## all this code should work fine if you acceessed it via the R Project named "alcatraz_hep". If you simply opened the R code file named "alcatraz", then you will have problems with file paths. 
+## 
 
 
-## the data files we get from USGS for Alcatraz are in a funky format.
-## they come in a xlsx file, with a sheet for each species (but 2018 data had both species in 1 sheet)
-## in each species sheet, there are 3 rows at the top before the data actually start, with the column names occupying the 2nd and 3rd rows (yes, column names spanning 2 rows)
-## then the actual nest check data are arranged in a wide format, with a row for each nest and then repeating sets of columns for each nest check (e.g. date, nest contents, etc) stacked horizontally out as far as needed to match the number of checks for each nest
-## the number of checks varies between species and between individual nests, so the code below reactive to the nest-specific number of checks
+# all_checks_usgs <- read.xlsx(here("data/alcatraz/70_Alcatraz_2021databackupSNG,BCNH.xlsx"), sheetIndex = "Visits", startRow = 3)
 
+# names(all_checks_usgs) <- tolower(names(all_checks_usgs))
 
-# load the required packages
-library(tidyverse) 
-library(lubridate)
-library(xlsx)
-library(stringr)
-library(here)
+#' Pivot funky format USGS data to long format
+#'
+#' @param usgs_checks data frame from USGS provided .csv or .xlsx. has a record for each nest observed (multiple observation days per row)
+#'
+#' @return data frame with a record for each nest for each observation day
+#' @export  
+#' 
+#' @details The data files we get from USGS for Alcatraz are in a funky format. They come in a xlsx file, with a sheet for each species (but 2018 data had both species in 1 sheet). In each species sheet, there are 3 rows at the top before the data actually start, with the column names occupying the 2nd and 3rd rows (i.e., column names spanning 2 rows). Then the actual nest check data are arranged in a wide format, with a row for each nest and then repeating sets of columns for each nest check (e.g. date, nest contents, etc) stacked horizontally out as far as needed to match the number of checks for each nest. The number of checks varies between species and between individual nests.
+#' 
+#'  This function pivots these data to a longer format with a record for each check of each nest, and it is reactive to the nest-specific number of checks. This takes advantage of the way R handles duplicated column names upon reading in data: adding sequential numbers to the end of column names (e.g. date, date.1, date.2, etc.). These trailing numbers can be used in dplyr::select() to isolate each observation day. 
+#'
+#' @examples 
+#' # read data
+#' all_checks_usgs <- read.xlsx(here("data/alcatraz/70_Alcatraz_2021databackupSNG,BCNH.xlsx"), sheetIndex = "Visits", startRow = 3) 
+#' 
+#' # names to lower case
+#' names(all_checks_usgs) <- tolower(names(all_checks_usgs)) 
+#' 
+#' long_alcatraz <- pivot_alcatraz(all_checks_usgs)
+pivot_alcatraz <- function(usgs_checks) {
+  
+names(usgs_checks) <- tolower(names(usgs_checks))
 
-
-
-all_checks_usgs <- read.xlsx(here("data/alcatraz/70_Alcatraz_2021databackupSNG,BCNH.xlsx"), sheetIndex = "Visits", startRow = 3)
-
-
-
-#--------------------------------------------------------
-
-names(all_checks_usgs) <- tolower(names(all_checks_usgs))
-
-alcatraz_checks <- all_checks_usgs %>% 
-  select(nest = no., spp, date, contains(c("egg", "chick", "age", "notes"))) %>%
+usgs_checks <- usgs_checks %>% 
+  rename(species = spp)
+# remove un-needed columns and make field types friendly  
+just_checks <- all_checks_usgs %>% 
+  select(nest = no., species, contains(c("date", "egg", "chick", "age", "notes"))) %>%
   mutate_all(as.character) %>% 
-  pivot_longer(cols = contains(c("egg", "chick", "age", "notes"))) %>% 
-  filter(!is.na(nest)) %>% 
-  separate(name, c("variable", "check.num")) %>% 
-  mutate(check.num = ifelse(is.na(check.num), 0, check.num),
-         check.num = as.numeric(check.num),
-         check.num = 1 + check.num) %>% 
-  pivot_wider(id_cols = c("nest", "spp", "date", "check.num"), names_from = variable, values_from = value) %>% 
-  filter(!is.na(egg) & !is.na(chick) & !is.na(age)) %>% 
-  # some egg, chick, age cells are "x" for renests; tis will turn those to NA, which is appropriate
-  mutate(date = ymd(date), across(.cols = c(egg, chick, age), as.numeric))
+  filter(!is.na(nest)) 
 
+# visit_cols is a vector representing the maximum number of observation days, to be used for slicing off each observation day.
+visit_cols = seq(1:(((ncol(just_checks)-2)/5) - 1))
+visit_cols = as.character(visit_cols)
+
+# this slices off the columns for each observation day, plus the species and nest id columns
+get_visit_cols <- function(col_nums) {
+  alc_visit <- just_checks %>% 
+    select(nest, species, ends_with(col_nums)) %>% 
+    # there's probably a way to gsub(col_nums) to do this rename in one line, but getting the evaluation is likely tricky and I didn't try. this works.
+    rename(date = contains("date"),
+           egg = contains("egg"),
+           chick = contains("chick"),
+           age = contains("age"),
+           notes = contains("notes"))
+  return(alc_visit)
+}
+
+# loop through get_visit_cols on each element of visit_cols and stack them vertically
+all_visit_cols <- map_df(visit_cols, get_visit_cols)
+
+# do a bit of basic cleaning
+long_alcatraz <- just_checks %>% 
+  select(nest, species, date, egg, chick, age, notes) %>% 
+  bind_rows(all_visit_cols) %>% 
+  filter(!is.na(egg) & !is.na(chick) & !is.na(age)) %>% 
+  # some egg, chick, age cells are "x" for renests; this will turn those to NA, which is appropriate
+  mutate(date = ymd(date), across(.cols = c(egg, chick, age), as.numeric)) %>% 
+  arrange(species, nest, date)
+
+return(long_alcatraz)
+
+}
 
 
 # some basic data checking ----
 
-summary(alcatraz_checks)
+# summary(long_alcatraz)
 # any unexpected species?
-count(alcatraz_checks, spp)
+# count(long_alcatraz, species)
 
 # notes?
-distinct(alcatraz_checks, notes) %>% view()
+# distinct(long_alcatraz, notes) %>% view()
 
 
 # fix any problems ----
-alcatraz_checks <- alcatraz_checks %>% 
-  filter(spp != "PASS")
+# Remove from 2021 a "passerine" nest that was monitored for fun.
+#long_alcatraz <- long_alcatraz %>% filter(species != "PASS")
 
 
-#
-## extract usefull info from notes; this likely not needed for hepraw_to_HEPDATA ----
 
-notes_extracter <- function(sp_checks){
-  # fill the egg or chick fields with data extracted from the notes field, as appropriate
-  # many records with no info in egg or chick are nonetheless valuable because the notes specify that the nest was empty (egg and chick = 0); is valuable info for nest screening, allowing a nest's record to end with an affirmative failure rather than just no data
-  # this function fills in egg and chick for some of the most common such occurences
-  # warnings about NAs introduced by coercion are OK
-  # this function adds a 'keeper' field, which flags records as having real data that we want to keep, or as being empty records that are relicts of the USGS table structure
-  # the user can double check the records with keeper == "N" in the output to make sure the rules for exclusion worked correctly for the current data file
-  # alc_noter() below can help ID new records (with different notes) that may need to be included in this function
-  # INPUT: sp_checks is the species-specific df created by alcatraz_nest_checker()
-
-  sp_checks2 <- alcatraz_checks %>%  
+#' Extract info from notes to fill the egg or chick fields with data extracted from the notes field, as appropriate
+#'
+#' @param long_alcatraz data frame with pivoted-long alcatraz data; output from pivot_alcatraz().
+#'
+#' @return data frame with similar structure as long_alcatraz, but with "keeper" field added, see details.
+#' @export
+#' 
+#' @details Many records with egg or chick blank are nonetheless valuable because the notes specify that the nest was empty (so egg and chick should be 0); is valuable info for nest screening, allowing a nest's record to end with an affirmative failure rather than just no data. This function fills in egg and chick for some of the most common such occurrences.
+#' 
+#' Warnings about NAs introduced by coercion are OK
+#' 
+#' This function adds a 'keeper' field, which flags records as having real data that we want to keep, or as being empty records that are relics of the USGS table structure.
+#' 
+#' The user can then double check the records with keeper == "N" in the output to make sure the rules for exclusion worked correctly for the current data file. 
+#'
+#' @examples all_checks_extracted <- notes_extracter(long_alcatraz)
+notes_extracter <- function(long_alcatraz){
+  sp_checks2 <- long_alcatraz %>%  
   mutate(notes.failed = ifelse(
                           #(is.na(egg) & is.na(chick)) & 
                          ((str_detect(notes, "empty") & !str_detect(notes, "missed")) | 
@@ -101,67 +135,80 @@ notes_extracter <- function(sp_checks){
   
   sp_checks4 <- sp_checks3 %>% 
     select(-egg, -chick) %>%
-    select(nest, spp, date, egg = egg2, chick = chick2, age, notes, notes.failed)  %>% 
+    select(nest, species, date, egg = egg2, chick = chick2, age, notes, notes.failed)  %>% 
     mutate(keeper = ifelse(is.na(egg) & is.na(chick), "N", "Y")) %>%
     arrange(nest, date) %>%
     distinct()
-                           
+return(sp_checks4)                           
 }
 
 
-all_checks_extracted<- notes_extracter(all_dups_Fixed)
-
-
-
-#--------------------------------------------------------
-## IMPORTANT MANUAL STEP HERE: examine the ...checks_extracted outputs to make sure no valuable/valid records have been classified with keeper == "N". 
-
-#Probably just need to look at records that have notes
-alcatraz_checks %>% 
-    filter(keeper == "N", !is.na(notes)) %>% 
-  view()
-
-
-# can do a manual edit if there is still good data in alc_keeperN_with_notes, changing keeper to Y and changing egg, chick, etc as appropriate
-all_checks_extracted <- edit(all_checks_extracted)
-
-  # a helper to check what the most common notes are and compare to the notes that are specified in notes_extracter() to make sure we're dealing with the important ones
+#' Export Alcatraz notes
+#'
+#' @param sp_checks data frame in long format with a notes field. Could be the output of either pivot_alcatraz or notes_extractor. 
+#' @param zyear 
+#'
+#' @return nothing, just saves .csv to the season_summary_forms folder for zyear
+#' @export
+#'
+#' @details filters to all non-NA notes and exports them as a .csv to the year appropriate season summary forms folder
+#'
+#' @examples
+#' export_alcatraz_notes(all_checks_extracted)
+export_alcatraz_notes <- function(sp_checks, zyear) {
 all_checks_extracted %>% 
+  filter(!is.na(notes)) %>% 
+  distinct(species, date, notes) %>% 
+  write.csv(here(paste("season_summary_forms/", zyear, "/Alcatraz_usgs_notes.csv", sep = "")), row.names = FALSE)
+}
+
+
+check_notes_frequency <- function() {
+  all_checks_extracted %>% 
   select(notes) %>% 
   mutate(notes = sub("\\.$", "", notes),
          notes = tolower(notes)) %>% 
   table() %>% 
   data.frame() %>% 
-  arrange(-Freq)
-
+  arrange(-Freq) %>% 
+  view()
+}
 
 # and now filter to have just the keepers
-  all_checks_extracted <- all_checks_extracted %>% 
-    filter(keeper == "Y") %>% 
-    select(-keeper)
+#  all_checks_extracted <- all_checks_extracted %>% 
+#    filter(keeper == "Y") %>% 
+#    select(-keeper)
 
 
 
 
 # assign nest stage -----
 
-alcatraz_assign_stage <- function(sp_checks){
-  # finally convert check data from the Alcatraz format (massaged by above functions) to the HEP_screening format, including addition and filling of HEP-specific fields (status, stage, confidence)
-  # note that there are some species-specific assumptions built in to the generation of Stage
-  # if GREG or GBHE ever nest on Alcatraz the assignment of stages in this function will need to be changed to include stage 3
+#' Assign Alcatraz nest stages
+#' 
+#' Much of HEP data is based on nest stages, which are not recorded by USGS Alcatraz observers. This function assigns each nest to stage based on USGS-estimated chick age. Thus stage for Alcatraz BCNH and SNEG are not exactly the same as for all other HEP data.
+#'
+#' @param checks_extracted data frame with Alcatraz check data. this should generally be the output of all_checks_extracted()
+#'
+#' @return data frame with same structure as checks_extracted, but with stage field added.
+#' @export
+#' 
+#' @details 
+#' The generation of Stage is species-specific. This function currently only handles BCNH and SNEG. If we get USGS data on other species the assignment of stages in this function will need to be changed to accommodate those species.
+
+#' 
+#' @examples alcatraz_checks_stages <- alcatraz_assign_stage(all_checks_extracted)
+alcatraz_assign_stage <- function(checks_extracted){
 
 # create a small df with some of the nest stage boundaries
-stages <- data.frame(spp = as.character(c("BCNH", "SNEG")),
+stages <- data.frame(species = as.character(c("BCNH", "SNEG")),
                      end.stg2 = c(10, 10),
                      end.stg4 = c(15, 14))  
-
-zsp_checks <- alcatraz_checks %>% 
-  left_join(., stages, by = c("spp"))
-
-
+# add it to the check data
+zsp_checks <- checks_extracted %>% 
+  left_join(., stages, by = c("species"))
 
 # now the meat of the function to assign stages
-
 hep_checks <- zsp_checks %>% 
   mutate(stage = as.numeric(""),
          stage = ifelse((chick == 0 | is.na(chick)) & egg > 0, 1, stage),
@@ -172,168 +219,84 @@ return(hep_checks)
 }
 
 
-alcatraz_checks_stages <- alcatraz_assign_stage(alcatraz_checks)
 
-alcatraz_checks_stages <- alcatraz_checks_stages %>% 
-  rename(species = spp)
-# now converting to wrangled format ----
-# first the dates table ----
-# "date"
-# "code"
-# "multiple.survey.num"
-# "start"
-# "end"
-# "num.surveys.this.date"
-# "complete.count" 
+  
+  
 
-dates <- alcatraz_checks_stages %>% 
+
+#' Wrangle Alcatraz data
+#'
+#' @param checks_stages 
+#'
+#' @return a list with 8 elements to match the other wrangled_ objects: dates, observers.effort, nests, stages, brood.sizes, predators, disturbance, notes
+#' @export
+#' 
+#'  @details some info is not collected at all by USGS observers (start and end times), or only exists in by-nest notes (e.g., disturbance, predator info). So wrangled_alcatraz ends up having a lot of blank cells. Some of this is to be filled manually during Season Summary sheet screening.
+#' 
+#' @examples
+wrangle_alcatraz <- function(checks_stages) {
+#
+dates <- checks_stages %>% 
   distinct(date, species) %>% 
-  mutate(code = 70,
+  mutate(code = 70.888,
          multiple.survey.num = "",
          start = NA,
          end = NA,
          num.surveys.this.date = NA,
-         complete.count = NA)
+         complete.count = "no") %>%
+  mutate(date = as.Date(date),
+         multiple.survey.num = as.numeric(multiple.survey.num))
 
-
-# then observers.effort ----
-# "code"
-# "colony"
-# "observers"
-# "total.days"
-# "total.surveys"
-# "total.hours"
-
-observers_effort <- alcatraz_checks_stages %>% 
+#
+observers_effort <- checks_stages %>% 
   distinct(date, species) %>% 
   count(species) %>% 
   rename(total.days = n) %>% 
-  mutate(code = 70,
+  mutate(code = 70.888,
          colony = "Alcatraz Island",
-         observers = NA,
+         observers = "USGS/NPS",
          total.surveys = total.days,
          total.hours = NA)
 
-
-
-# nests ----
-# "code"
-# "date"
-# "multiple.survey.num"
-# "species"
-# "total.nests"
-# "complete.count"
-# "peak.active"
-# "obs.initials"
-
-nests <- alcatraz_checks_stages %>% 
-  filter(!is.na(stage)) %>% 
+# 
+nests <- checks_stages %>%  
+  filter(!is.na(stage), (egg > 0 | chick > 0)) %>% 
   count(date, species, name = "total.nests") %>% 
   group_by(species) %>% 
   mutate(peak.active = ifelse(total.nests == max(total.nests), TRUE, FALSE),
-         code = 70,
+         code = 70.888,
          multiple.survey.num = 1,
          complete.count = "no",
-         obs.initials = NA) %>% 
+         obs.initials = NA,
+         date = as.Date(date)) %>% 
   ungroup() %>% 
   arrange(species, date)
   
-  
-
-
-# stages ----
 # no stages for BCNH, SNEG
-
-
-# brood.sizes ----
 # no brood sizes for BCNH, SNEG
 
+#
+predators <- data.frame(date = as.Date(NA),
+                        predator.species = "",
+                        present = as.double(""),
+                        nesting = as.double(""),
+                        code = 70.888,
+                        multiple.survey.num = 1)
 
+#
+disturbance <- data.frame(date = as.Date(NA),
+                          obs.inf = "",
+                          type = "",
+                          result = "",
+                          description = "",
+                          code = 70.888,
+                          multiple.survey.num = "1")
 
-# for predators, disturbance and notes ----
-all_notes <- alcatraz_checks_stages %>% 
-  filter(!is.na(notes)) %>% 
-  distinct(date, species, notes) %>% view()
-
-
-# predators ----
-# "code"
-# "date"
-# "multiple.survey.num"
-# "species"
-# "predator.species"
-# "present"
-# "nesting"
-
-# if notes suggest predators, run this and change predator.note to 1 for that record and fill in predator.species, present and nesting as appropriate:
-predators <- all_notes %>% 
-  mutate(predator.note = 0,
-         predator.species = "",
-         present = "",
-         nesting = "",
-         code = 70,
-         multiple.survey.num = 1) %>% 
-  edit()
-
-predators <- predators %>% 
-  filter(predator.note == 1) %>% 
-  select(-predator.note)
-  
-# if no predators:
-predators = NULL
-
-# disturbance ----
-# "code"
-# "date"
-# "multiple.survey.num"
-# "species"
-# "obs.inf"
-# "type"
-# "result"
-# "description"
-
-
-# if notes suggest predators, run this and change predator.note to 1 for that record and fill in obs.inf, type, result, and description as appropriate:
-disturbance <- all_notes %>% 
-  mutate(disturbance.note = 0,
-         obs.inf = "",
-         type = "",
-         result = "",
-         description = "",
-         code = 70,
-         multiple.survey.num = 1) %>% 
-  edit()
-# then keep just the good records
-disturbance <- disturbance %>% 
-  filter(disturbance.note == 1) %>% 
-  select(-disturbance.note)
-  
-# if no disturbance:
-  disturbance = NULL
-
-
-# notes ----
-# "code"
-# "date"
-# "multiple.survey.num"
-# "species"
-# "note.type"
-# "notes"
-
-
-# if there are pertinent notes to keep, run this and change keep.note to 1 for that record and fill in note.type and edit the note as appropriate:
-notes <- all_notes %>% 
-  mutate(keep.note = 0,
-         note.type = "",
-         code = 70,
-         multiple.survey.num = 1) %>% 
-  edit()
-# then keep just the good records
-notes <- notes %>% 
-  filter(keep.note == 1) %>% 
-  select(-keep.note)
-# if no notes:
-notes = NULL
+#
+notes <- data.frame(date = as.Date(NA),
+                    note.type = "",
+                    code = 70.888,
+                    multiple.survey.num = 1)
 
 wrangled_alcatraz <- list(dates = dates,
                       observers.effort = observers_effort,
@@ -344,6 +307,7 @@ wrangled_alcatraz <- list(dates = dates,
                       disturbance = disturbance,
                       notes = notes)
 
-wrangled_alcatraz %>% 
-  saveRDS(here("data/wrangled/wrangled_alcatraz_2021"))
+return(wrangled_alcatraz)
+}
+
 
